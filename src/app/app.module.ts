77d881c6b2fe87185f4e9a7a9a68ba056dff57b0
Tiago Silva <esteprowork@gmail.com>
2019-03-29 10:43:24 +0100

    k

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {
+        path: 'inscription',
+        component: FormComponent,
+        data: {title: 'Inscription'}
+      },
+      {
+        path: '',
+        component: HomeComponent,
+        data: {title: 'Bikesterdam'}
+      }
+    ]),
+    MDBBootstrapModule.forRoot()
+    
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
